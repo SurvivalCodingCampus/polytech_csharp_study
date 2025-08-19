bc8614aa5d772a7d8a01bc6bf0965782492d86f8
Soso-Kiki <polytechid10@gmail.com>
2025-08-19 14:31:10 +0900

    feat: main함수 작성

---
+++ CsharpStudy.2Semester/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Text;
+
+namespace CsharpStudy._2Semester;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Word word = new Word("String");
+        Console.Write("1부터 i번째 글자 중 알기 원하는 번호를 작성해주세요. : ");
+
+        int input = Convert.ToInt32(Console.ReadLine());
+        string result = (word.IsVowel((input - 1))) ? "모음" : "자음";
+
+        Console.Write($"{input}번째 글자는 '{result}' 입니다");
+    }
+}
