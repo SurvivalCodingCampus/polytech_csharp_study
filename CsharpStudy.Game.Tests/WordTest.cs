616bc219bc8ef2e089be354fe722eada0be09250
Hae1Won1 <h121581@naver.com>
2025-08-19 12:46:34 +0900

    feat : Word클래스 Test 코드 작성

---
+++ CsharpStudy.Game.Tests/WordTest.cs
@@ -0,0 +1,28 @@
+﻿using CsharpStudy;
+using NUnit.Framework;
+
+namespace CsharpStudy.Game.Tests;
+
+[TestFixture]
+[TestOf(typeof(Word))]
+public class WordTest
+{
+    [Test]
+    public void 모음확인()
+    {
+        Word word = new Word("abc");
+
+        Assert.That(word.isVowel(0),Is.True);
+        Assert.That(word.isVowel(1),Is.False);
+        Assert.That(word.isVowel(2),Is.False);
+    }
+    [Test]
+    public void 자음확인()
+    {
+        Word word = new Word("abc");
+
+        Assert.That(word.isConsonant(0),Is.False);
+        Assert.That(word.isConsonant(1),Is.True);
+        Assert.That(word.isConsonant(2),Is.True);
+    }
+}
