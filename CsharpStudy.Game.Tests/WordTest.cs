d015d480d6376416b7f7529e85299c8c334a34bf
Soso-Kiki <polytechid10@gmail.com>
2025-08-19 18:50:18 +0900

    feat: Word 클래스 test 코드 수정

--- CsharpStudy.Game.Tests/WordTest.cs
+++ CsharpStudy.Game.Tests/WordTest.cs
@@ -11,16 +11,26 @@ public class WordTest
     [Test]
     public void IsVowelAtIndex_ShouldReturnTrue()
     {
-        Word word = new Word("StringTestCode");
-        bool result = word.IsVowel(7);
-        Assert.That(result, Is.True);
+        Word word = new Word("aeiou");
+        int length = word.Text.Length;
+        for (int i = 0; i < length; i++)
+        {
+            bool result = word.IsVowel(i);
+            Assert.That(result, Is.True);
+        }
+       
+       
         
     }
     [Test]
     public void IsConsonantAtIndex_ShouldReturnTrue()
     {
-        Word word = new Word("StringTestCode");
-        bool result = word.IsConsonant(4);
-        Assert.That(result, Is.True);
+        Word word = new Word("bcdfghjklmnpqrstvwxyz");
+        int length = word.Text.Length;
+        for (int i = 0; i < length; i++)
+        {
+            bool result = word.IsConsonant(i);
+            Assert.That(result, Is.True);
+        }
     }
 }
