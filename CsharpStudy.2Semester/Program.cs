ed75316f0b817dcfe79f87518a6fb5770fdacec0
Soso-Kiki <polytechid10@gmail.com>
2025-08-19 16:48:23 +0900

    feat: main 함수 사용자 입력값에 대한 유효성 검사 코드 추가

--- CsharpStudy.2Semester/Program.cs
+++ CsharpStudy.2Semester/Program.cs
@@ -10,9 +10,15 @@ class Program
     static void Main(string[] args)
     {
         Word word = new Word("String");
+        int length = (word.Text.Length + 1);
         Console.Write("1부터 i번째 글자 중 알기 원하는 번호를 작성해주세요. : ");
 
         int input = Convert.ToInt32(Console.ReadLine());
+        if (input <= 0 || length < input)
+        {
+            return;
+        }
+
         string result = (word.IsVowel((input - 1))) ? "모음" : "자음";
 
         Console.Write($"{input}번째 글자는 '{result}' 입니다");
