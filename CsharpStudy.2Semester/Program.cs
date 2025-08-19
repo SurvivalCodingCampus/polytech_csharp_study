74a7a8a735ce77a44c14075c9b1c172f1ab73c17
Soso-Kiki <polytechid10@gmail.com>
2025-08-19 17:11:23 +0900

    feat: main 함수 > TryParse를 이용하여 문자열을 정수로 안전 변환, 유효성 범위 체크

--- CsharpStudy.2Semester/Program.cs
+++ CsharpStudy.2Semester/Program.cs
@@ -11,16 +11,25 @@ class Program
     {
         Word word = new Word("String");
         int length = (word.Text.Length + 1);
-        Console.Write("1부터 i번째 글자 중 알기 원하는 번호를 작성해주세요. : ");
+        Console.Write("$1 이상 {word.Text.Length} 이하의 숫자를 입력하세요");
 
-        int input = Convert.ToInt32(Console.ReadLine());
-        if (input <= 0 || length < input)
+        string? line = Console.ReadLine(); // 콘솔 입력 = 문자열로 들어옴 + null 값 포함
+        // 유효성 검사 / out : 메서드에 매개변수 전달시, 해당 매개변수가 참조로 전달되로고 지정 키워드
+        if (!int.TryParse(line, out int input)) // 문자열 -> 정수로 안전 변환 시도 
         {
+            Console.WriteLine("유효한 숫자를 입력하세요.");
             return;
         }
 
-        string result = (word.IsVowel((input - 1))) ? "모음" : "자음";
+        if (input < 1 || input > word.Text.Length)
+        {
+            Console.WriteLine($"1 이상 {word.Text.Length} 이하의 숫자를 입력하세요.");
+            return;
+        }
 
-        Console.Write($"{input}번째 글자는 '{result}' 입니다");
+        // 범위 체크(사용자는 1부터 세니 '-1' 해주기)
+        bool isVowel = word.IsVowel(input - 1);
+        string result = isVowel ? "모음" : "자음";
+        Console.WriteLine($"{input}번째 글자는 '{result}' 입니다");
     }
 }
