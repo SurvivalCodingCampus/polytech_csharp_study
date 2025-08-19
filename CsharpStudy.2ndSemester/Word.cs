4cf09740b621ab0e4936a045e0762ca1138dbdf8
Lazylionr6 <syeop98@gmail.com>
2025-08-19 14:19:35 +0900

    2025.08.19_05_김상엽

---
+++ CsharpStudy.2ndSemester/Word.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace CsharpStudy._2ndSemester;
+
+public class Word
+{
+    public String word = "";
+    
+    public bool isVowel(int i)
+    {
+            
+        switch (word.ToLower()[i])
+        {
+            case 'a':
+                return true;
+            case 'e':
+                return true;
+            case 'i':
+                return true;
+            case 'o':
+                return true;
+            case 'u':
+                return true;
+            default: return false;
+        }
+    }
+
+    public bool isConsonant(int i)
+    {
+        if (isVowel(i) == true)
+        {
+            return false;
+        }
+        else if (word.ToLower()[i] - 'a' >=0 && word.ToLower()[i] - 'a' < 26)
+        {
+            return true;
+        }
+        else return false;
+    }
+}
