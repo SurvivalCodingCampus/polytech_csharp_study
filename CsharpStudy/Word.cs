275d6f38df3d603519b962ce5462dc5f5fc40af5
Hae1Won1 <h121581@naver.com>
2025-08-19 12:46:09 +0900

    feat : Word 클래스 생성

---
+++ CsharpStudy/Word.cs
@@ -0,0 +1,20 @@
+﻿namespace CsharpStudy;
+
+public class Word
+{
+    private string word = "";
+    public Word(string word)
+    {
+        this.word = word;
+    }
+
+    public bool isVowel(int i)
+    {
+        return word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u';;
+    }
+
+    public bool isConsonant(int i)
+    {
+        return !(word[i] == 'a' || word[i] == 'e' || word[i] == 'i'  || word[i] == 'o' || word[i] == 'u');
+    }
+}
