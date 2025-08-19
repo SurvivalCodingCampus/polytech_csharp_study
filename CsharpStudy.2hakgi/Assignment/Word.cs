538b844c8df7cbe27b220b05c4667e253490c1d9
jinsukuku <jinsukuku@gmail.com>
2025-08-19 15:20:01 +0900

    feat: Word 클래스 추가, IsVowel, IsConsonant 구현

---
+++ CsharpStudy.2hakgi/Assignment/Word.cs
@@ -0,0 +1,40 @@
+namespace CsharpStudy._2hakgi.Assignment;
+
+public class Word
+{
+    private string _word = "";
+
+    public Word(string word)
+    {
+        _word = word;
+    }
+
+    // i 번째 글자가 모음인지
+    public bool IsVowel(int i)
+    {   
+        // 범위 벗어나는지 확인
+        if (i < 0 || i >= _word.Length)
+        {
+            throw new ArgumentOutOfRangeException("인덱스가 문자열 범위를 벗어났습니다.");
+        }
+
+        // 알파벳인지 확인
+        char c = _word[i];
+        if('a' <= c && c <= 'z' && 'A' <= c && c <= 'Z')
+        {
+            throw new ArgumentException("문자가 알파벳이 아닙니다.");
+        }
+
+        // 소문자로 변경
+        c = char.ToLower(_word[i]); 
+        
+        // return
+        return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+    }
+    
+    // i 번째 글자가 자음인지
+    public bool IsConsonant(int i)
+    {
+        return !IsVowel(i);
+    }
+}
