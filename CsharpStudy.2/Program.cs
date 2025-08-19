beaf501fa28b7996c0cfd1fc3d42c4aeb148f418
MJ37031 <rlaalswl0520@naver.com>
2025-08-19 13:07:05 +0900

    문자열

---
+++ CsharpStudy.2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+
+namespace CsharpStudy._2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Hero hero1 = new Hero(name: "홍길동");
+        Hero hero2 = new Hero(name: "홍길동");
+
+        Console.WriteLine(hero1 == hero2);
+        Console.WriteLine(hero1.GetHashCode()==hero2.GetHashCode());
+        Console.WriteLine(hero1.Equals(hero2));
+        Console.WriteLine(object.ReferenceEquals(hero1, hero2));
+    }
+
+
+
+    static void StringPool()
+    {
+        string s1 = "HELLO";  // 동적으로 생김(new) 안 해도 생김
+        Console.WriteLine(s1.GetHashCode());
+        
+        string s2 = "HELLO";
+        Console.WriteLine(s2.GetHashCode());
+        
+    }
+
+    public static string GetLo()
+    {
+        return "LO";
+    }
+    
+    
+    static void String_vs_StringBuilder()
+    {
+        string s1 = "HELLO";  // 동적으로 생김(new) 안 해도 생김
+        Console.WriteLine(s1.GetHashCode());
+        s1 += " World";
+        Console.WriteLine(s1.GetHashCode());
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 100000; i++)
+        {
+            sb.Append(i); // 0.0018208초
+        }
+        stopwatch.Stop();
+        
+        Console.WriteLine($"끝 {stopwatch.Elapsed.TotalSeconds}");
+        //Console.WriteLine(s1 + " World");
+        //Console.WriteLine($"{s1} World");
+    }
+}
