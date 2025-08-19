92b34fa08ffe8ab5eeef1604b8359c9401e75f7c
KOO TAEHAN <137146632+OmaryKoo@users.noreply.github.com>
2025-08-19 13:57:40 +0900

    feat. csharp word class 연습문제1

---
+++ CsharpStudy.2Semester/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CsharpStudy._2Semester;
+
+class Program
+{
+    static void Main()
+    {
+        Word w = new Word();
+        bool result = w.isVowel(3);
+        Console.WriteLine(result);
+
+    }
+}
---
+++ CsharpStudy.2Semester/Word.cs
@@ -0,0 +1,18 @@
+﻿namespace CsharpStudy._2Semester;
+
+public class Word
+{
+    string word = "cshArp";
+
+    public bool isVowel(int i)
+    {
+        if (i < 0 || i >= word.Length)
+        {
+            return false;
+        }
+
+        char c = char.ToLower(word[i]);
+        string vowel = "aeiou";
+        return vowel.Contains(c); //문자열 or 컬렉션에서 특정 요소 찾을 때 사용 메서드
+    }
+}
