ee5de8ae51e37887ef04e9087af5aae1eef07282
Kangchanghwan <lgodl1598@naver.com>
2025-08-19 11:48:05 +0900

    init: 2학기 시작
    # Conflicts:
    #	CsharpStudy.Game/Characters/Cleric.cs
    #	CsharpStudy.sln

---
+++ CsharpStudy.2hakgi/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CsharpStudy._2hakgi;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string s1 = "HELLO";
+        Console.WriteLine(s1.Replace("LL", "XX"));
+    }
+}
