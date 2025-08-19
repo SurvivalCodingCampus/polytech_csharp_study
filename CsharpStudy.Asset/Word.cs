bf3f673d59f0fdb35eb8b4bfd6dc0991bd01540a
choiain <csh25tmd25@gmail.com>
2025-08-19 18:10:51 +0900

    feat: 과제 1번

---
+++ CsharpStudy.Asset/Word.cs
@@ -0,0 +1,22 @@
+namespace CsharpStudy.Asset;
+
+public class Word
+{
+    private string word;
+
+    public Word(string w)
+    {
+        word = w.ToLower();
+    }
+
+    public bool IsVowel(int i)
+    {
+        // i가 0보다 작거나 단어의 길이보다 크거나 같으면 false 
+        if (i < 0 || i >= word.Length)
+        {
+            return false;
+        }
+        
+        return word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u';
+    }
+}
