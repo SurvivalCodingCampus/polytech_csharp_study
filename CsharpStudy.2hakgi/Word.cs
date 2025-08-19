e60cebb2faeae14157ebf30670675b2f6c7c66a7
Kangchanghwan <lgodl1598@naver.com>
2025-08-19 13:21:24 +0900

    fix: 대문자 소문자 무시 test: 메서드 테스트 코드 추가

--- CsharpStudy.2hakgi/Word.cs
+++ CsharpStudy.2hakgi/Word.cs
@@ -12,7 +12,7 @@ public class Word
     public bool IsVowel(int i)
     {
         string vowel = "aeiou";
-        return vowel.IndexOf(_word.ElementAt(i)) > -1;
+        return vowel.IndexOf(_word.ToLower().ElementAt(i)) > -1;
     }
 
     public bool IsConsonant(int i)
