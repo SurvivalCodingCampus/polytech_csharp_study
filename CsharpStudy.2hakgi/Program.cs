fb6b86660800aa015c614d26350bfbb37443de7a
Kangchanghwan <lgodl1598@naver.com>
2025-08-19 11:55:27 +0900

    feat: 자음인지 모음인지 확인하는 클래스 구현

---
+++ CsharpStudy.2hakgi/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CsharpStudy._2hakgi;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Word word = new Word("goodle");
+
+        Console.WriteLine(word.IsVowel(2));
+        Console.WriteLine(word.IsVowel(0));
+        Console.WriteLine(word.IsConsonant(0));
+        Console.WriteLine(word.IsConsonant(2));
+        
+    }
+}
