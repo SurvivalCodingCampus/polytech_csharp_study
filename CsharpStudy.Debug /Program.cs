5efcfa037901494c043ad980de0fb9bdbc9ac54c
Soso-Kiki <polytechid10@gmail.com>
2025-08-25 13:02:18 +0900

    docs: 주석 추가

--- CsharpStudy.Debug /Program.cs
+++ CsharpStudy.Debug /Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         var controller = new YukymController();
 
-        Console.WriteLine($"GetTyA() 결과: {controller.GetTyA()}");
-        Console.WriteLine($"GetTyB() 결과: {controller.GetTyB()}");
+        Console.WriteLine($"GetTyA() 결과: {controller.GetTyA()}"); // month : '08' = 경오 4국  
+        Console.WriteLine($"GetTyB() 결과: {controller.GetTyB()}"); // Hour : '11' : 갑자 5국 
     }
 }
