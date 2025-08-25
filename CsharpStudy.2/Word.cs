9c674236bf9a7081ac07371048b0cbd3d88a0c93
Dayeong <204299917+iiijjjijij@users.noreply.github.com>
2025-08-25 10:08:45 +0900

    feat: IsVowel()에 i 범위 지정

--- CsharpStudy.2/Word.cs
+++ CsharpStudy.2/Word.cs
@@ -20,6 +20,11 @@ public class Word
     {
         // 대문자의 경우 소문자로 변경
         // Index0f: 문자열/문자 배열에서 특정 문자가 처음으로 나타나는 인덱스 반환. 존재하지 않으면 -1 반환
+
+        if (i < 0 || i >= word.Length)
+        {
+            throw new ArgumentException("문자열 길이를 벗어남");
+        }
         
         string vowel = "aeiou";
         char ch = Char.ToLower(word[i]);
