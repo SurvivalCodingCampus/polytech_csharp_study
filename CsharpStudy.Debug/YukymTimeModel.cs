1f45c8ec552ec347adf6169fc296c6773bb0057e
KOO TAEHAN <137146632+OmaryKoo@users.noreply.github.com>
2025-08-25 13:36:14 +0900

    feat.디버그 과제 수정 + TIL

--- CsharpStudy.Debug/YukymTimeModel.cs
+++ CsharpStudy.Debug/YukymTimeModel.cs
@@ -6,7 +6,7 @@ public class YukymTimeModel
     public string Ty2 { get; set; } = "갑자2국";
     public string Ty3 { get; set; } = "갑자3국";
     public string Ty4 { get; set; } = "갑자4국";
-    public string Ty5 { get; set; } = "갑자5국";
+    public string Ty5 { get; set; } = "갑자5국"; 
     public string Ty6 { get; set; } = "갑자6국";
     public string Ty7 { get; set; } = "갑자7국";
     public string Ty8 { get; set; } = "갑자8국";
