c8c0359990cabee1d01d8874017409be76a2715f
MJ37031 <rlaalswl0520@naver.com>
2025-08-26 15:46:31 +0900

    feat: 과제

--- CsharpStudy_FP2/Trader/Trager.cs
+++ CsharpStudy_FP2/Trader/Trager.cs
@@ -43,7 +43,8 @@ public class MainClass
 
     public static void Main(string[] args)
     {
-        // 1. 2011년에 일어난 모든 트랜잭션을찾아 가격 기준 오름차순으로정리하여이름을 나열하시오
+        // 1. 2011년에 일어난 모든 트랜잭션을찾아 가격 기준 오름차순으로 정리하여 이름을 나열하시오
+        Console.WriteLine("1. 2011년에 일어난 모든 트랜잭션을찾아 가격 기준 오름차순으로 정리하여 이름을 나열하시오");
         transactions.Where(transaction => transaction.Year == 2011)
             .OrderBy(transaction => transaction.Value)
             .Select(transaction => transaction.Trader.Name)
@@ -51,25 +52,52 @@ public class MainClass
             .ForEach(Console.WriteLine);
         
         // 2. 거래자가 근무하는 모든 도시를 중복 없이 나열하시오
+        Console.WriteLine("2. 거래자가 근무하는 모든 도시를 중복 없이 나열하시오");
+        transactions.Select(transaction => transaction.Trader.City)
+            .ToHashSet()
+            .ToList()
+            .ForEach(Console.WriteLine);
         
-        
-        // 3. 케임브리지에서 근무하는 모든 거래자를찾아서 이름순으로 정렬하여 나열하시오
-        
+        // 3. 케임브리지에서 근무하는 모든 거래자를 찾아서 이름순으로 정렬하여 나열하시오
+        Console.WriteLine("3. 케임브리지에서 근무하는 모든 거래자를 찾아서 이름순으로 정렬하여 나열하시오");
+        transactions.Where(transaction => transaction.Trader.City == "Cambridge")
+            .OrderBy(transaction => transaction.Trader.Name)
+            .Select(transaction => transaction.Trader.Name)
+            .ToHashSet()
+            .ToList()
+            .ForEach(Console.WriteLine);
         
         // 4. 모든 거래자의 이름을 알파벳순으로 정렬하여 나열하시오
-        
+        Console.WriteLine("4. 모든 거래자의 이름을 알파벳순으로 정렬하여 나열하시오");
+        transactions.OrderBy(transaction => transaction.Trader.Name)
+            .Select(transaction => transaction.Trader.Name)
+            .ToHashSet()
+            .ToList()
+            .ForEach(Console.WriteLine);
 
         // 5. 밀라노에 거래자가 있는가?
+        Console.WriteLine("5. 밀라노에 거래자가 있는가?");
+        Console.WriteLine(transactions.Any(t => t.Trader.City == "Milan"));
         
 
         // 6. 케임브리지에 거주하는 거래자의 모든 트랙잭션 값을 출력하시오
-        
+        Console.WriteLine("6. 케임브리지에 거주하는 거래자의 모든 트랙잭션 값을 출력하시오");
+        transactions.Where(transaction => transaction.Trader.City == "Milan")
+            .Select(transaction => transaction.Value)
+            .ToList()
+            .ForEach(Console.WriteLine);
         
         // 7. 전체 트랜잭션중 최대값은 얼마인가?
-        
-        
+        Console.WriteLine("7. 전체 트랜잭션중 최대값은 얼마인가?");
+        Console.WriteLine(transactions.Select(transaction => transaction.Value)
+            .Max());
+
+
         // 8. 전체 트랜잭션중 최소값은 얼마인가?
-        
-        
+        Console.WriteLine("8. 전체 트랜잭션중 최소값은 얼마인가?");
+        Console.WriteLine(transactions.Select(transaction => transaction.Value)
+            .Min());
+
+
     }
 }
