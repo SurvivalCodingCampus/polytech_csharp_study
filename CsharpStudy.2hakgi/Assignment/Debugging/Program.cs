1e19cbcdaa809e0c237ed9ab0bc4f357152cbbfb
jinsukuku <jinsukuku@gmail.com>
2025-08-26 11:42:44 +0900

    chore: Debugging 과제 Main문 주석 처리

--- CsharpStudy.2hakgi/Assignment/Debugging/Program.cs
+++ CsharpStudy.2hakgi/Assignment/Debugging/Program.cs
@@ -2,6 +2,7 @@ namespace CsharpStudy._2hakgi.Assignment.Debugging;
 
 class Program
 {
+    /*
     public static void Main(string[] args)
     {
         var controller = new YukymController();
@@ -9,4 +10,5 @@ class Program
         Console.WriteLine($"GetTyA() 결과: {controller.GetTyA()}");
         Console.WriteLine($"GetTyB() 결과: {controller.GetTyB()}");
     }
+    */
 }
