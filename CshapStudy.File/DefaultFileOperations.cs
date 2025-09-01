5ae2dbb8771d18c859b1883b93aaf2f3fe917d33
Hae1Won1 <h121581@naver.com>
2025-09-01 14:44:30 +0900

    feat : DefaultFileOperations 클래스 작성

---
+++ CshapStudy.File/DefaultFileOperations.cs
@@ -0,0 +1,24 @@
+﻿namespace CshapStudy.File;
+
+public interface IFileCopier
+{
+     public void CopyFile(string sourceFilePath, string destinationFilePath);
+}
+
+public class DefaultFileOperations : IFileCopier
+{
+     // 월본 파일 경로, 복사할 파일 경로
+     public void CopyFile(string sourceFilePath, string destinationFilePath)
+     {
+          try
+          {
+               string text = System.IO.File.ReadAllText(sourceFilePath);
+               System.IO.File.WriteAllText(destinationFilePath, text);
+          }
+          catch(IOException e)
+          {
+               Console.WriteLine(e.Message);
+               Console.WriteLine("경로를 다시 지정해주세요");
+          }
+     }
+}
