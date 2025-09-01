0cac83b8ae94265d312942b047a9ee07233fef5a
MJ37031 <rlaalswl0520@naver.com>
2025-09-01 14:47:27 +0900

    feat: 연습문제2 예외가 발생하면 num을 0으로 처리

---
+++ CsharpStudy.Exception/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace CsharpStudy.Exception;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var numstring = "10.5";
+        
+        try
+        {
+            int num = int.Parse(numstring);
+            Console.WriteLine(num);
+        }
+
+        catch (System.Exception e)
+        {
+            int num = 0;
+            Console.WriteLine(num);
+        }
+    }
+}
+
+
