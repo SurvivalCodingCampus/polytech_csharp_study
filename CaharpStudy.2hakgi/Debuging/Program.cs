ff5e62987c5f33961f61bb4ed66d12af8e571694
namyejin <namye6950@gmail.com>
2025-09-01 14:37:03 +0900

    feat: 예외와 파일 조작

--- CaharpStudy.2hakgi/Debuging/Program.cs
+++ CaharpStudy.2hakgi/Debuging/Program.cs
@@ -2,13 +2,13 @@ namespace CaharpStudy._2hakgi.Debuging;
 
 public class Program
 {
-    public static void Main(string[] args)
+   /* public static void Main(string[] args)
     {
         var controller = new YukymController();
 
         Console.WriteLine($"GetTyA() 결과: {controller.GetTyA()}");
         Console.WriteLine($"GetTyB() 결과: {controller.GetTyB()}");
         
-    }
+    }*/
     
 }
---
+++ CaharpStudy.2hakgi/File/IFileCopier.cs
@@ -0,0 +1,6 @@
+namespace CaharpStudy._2hakgi.File;
+
+public interface IFileCopier
+{
+    public void CopyFile(string source, string destination);
+}
