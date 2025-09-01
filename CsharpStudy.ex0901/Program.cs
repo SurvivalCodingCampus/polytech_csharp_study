6e31e195173f24f6c289ce2cabd6f41d63cee152
Hae1Won1 <h121581@naver.com>
2025-09-01 13:36:57 +0900

    feat : 9장 연습문제 수정

---
+++ CsharpStudy.ex0901/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CsharpStudy.ex0901;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var numString = "10.5";
+        int num;
+        try
+        {
+            num = int.Parse(numString);
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine(e.Message);
+            num = 0;
+        }
+        Console.WriteLine(num);
+    } 
+}
