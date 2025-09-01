ed5a31c404e0bc52a57b905fa5533dff43579b03
Dayeong <204299917+iiijjjijij@users.noreply.github.com>
2025-09-01 11:40:21 +0900

    first: 예외 처리

---
+++ CsharpStudy.Exception_File/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CsharpStudy.Exception_File;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var numString = "10.5";
+        int num=int.Parse(numString);
+        
+        Console.WriteLine(num);
+        // FormatException 발생
+        // int.Parse()는 정수 문자열만 받아들임
+    }
+}
