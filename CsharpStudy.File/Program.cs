f44cfa49a497e42cb7d2a1535221abee8501a4e0
MJ37031 <rlaalswl0520@naver.com>
2025-09-01 14:12:05 +0900

    feat: 파일을 복서하는 DefaultFileOperations 작성

--- CsharpStudy.File/Program.cs
+++ CsharpStudy.File/Program.cs
@@ -1,29 +1,40 @@
 ﻿namespace CsharpStudy.File;
 
-class Program
+class Program : IFileCopier
 {
-    static void Main(string[] args)
+    public void CopyFile(string sourceFilePath, string destinationFilePath)
     {
-        string text = "안녕 홍길동\n";
-        
+        string text = System.IO.File.ReadAllText(sourceFilePath);
+        System.IO.File.WriteAllText(destinationFilePath, text);
+    }
+
+    static void Main(string[] args)
+    { 
+        IFileCopier copier = new Program(); 
+        copier.CopyFile("test.txt", "result.txt");
+
+        /*string text = "안녕 홍길동\n";
+
         System.IO.File.WriteAllText("test.txt", text);  // 알아서 열고 닫아줌
         System.IO.File.WriteAllText("test.txt", text);  // 갈아엎어짐
 
        System.IO.File.AppendAllText("test.txt", text);  // 뒤에 추가
        System.IO.File.AppendAllText("test.txt", text);
-       
+
        // text = System.IO.File.ReadAllText("test.txt");
        // Console.WriteLine(text);
-       
-       
+
+
        // 읽어오기
        // string[] lines = System.IO.File.ReadAllLines("test.txt");
        // lines.ToList().ForEach(Console.WriteLine);
-       
+
        string totalText = System.IO.File.ReadAllLines("text.txt")
            .Select(line => line.Replace("안녕", "Hello"))
            .Aggregate((a, b) => a + b);
-       Console.WriteLine(totalText);
+       Console.WriteLine(totalText);*/
 
     }
+
 }
+
