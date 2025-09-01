1e08d3050db116111da2c5239964dbc0d1cecfa5
Lazylionr6 <syeop98@gmail.com>
2025-09-02 04:11:06 +0900

    예외처리/파일조작

---
+++ CsharpStudy.Exception/Exception.cs
@@ -0,0 +1,26 @@
+﻿namespace CsharpStudy.Exception;
+using System;
+
+public class Exception
+{
+    static void Main(string[] args)
+    {
+        var numString = "10.5";
+        int num;
+        try
+        {
+            num = int.Parse(numString);
+            Console.WriteLine(num);
+        }
+        catch (FormatException e)
+        {
+            num = 0;
+        }
+    }
+}
+/*
+@30328
+throw new FormatException(SR.Format(SR.Format_InvalidStringWithValue, value.ToString()));
+
+the input string '10.5' was not in a correct format
+ */
---
+++ CsharpStudy.FileRW/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CsharpStudy.FileRW;
+
+public class Program
+{
+    public interface IFileCopier
+    {
+        void CopyFile(string sourceFilePath, string destinationFilePath);
+    }
+    
+    
+}
