d9f67aacfd7fd9ebaf43c127cf5df0c0f179e74c
MJ37031 <rlaalswl0520@naver.com>
2025-09-01 14:20:56 +0900

    feat: Exception 연습문제 1

---
+++ Exception/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CsharpStudy.Exception;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var numstring = "10.5";
+        int num = int.Parse(numstring);
+        Console.WriteLine(num);  // 자료형 
+    }
+}
