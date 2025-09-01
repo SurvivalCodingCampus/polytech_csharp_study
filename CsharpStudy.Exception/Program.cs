8c9bc28560d14b3c08c670e32b8510061f1fe74b
KOO TAEHAN <137146632+OmaryKoo@users.noreply.github.com>
2025-09-01 13:01:02 +0900

    feat.예외_연습문제2

--- CsharpStudy.Exception/Program.cs
+++ CsharpStudy.Exception/Program.cs
@@ -1,11 +1,22 @@
-﻿namespace CsharpStudy.Exception;
+﻿using System;
+namespace CsharpStudy.Exception;
 
 class Program
 {
-    static void Main(string[] _)
+    static void Main(string[] args)
     {
-        var numString = "10.5"; // 소수점 문자열
-        int num = int.Parse(numString); 
-        Console.WriteLine(num); // 도달하지 못함
+        var numString = "10.5";
+        int num;
+
+        try
+        {
+            num = int.Parse(numString);
+        }
+        catch (System.Exception) // Exception을 클래스가 아니라 네임스페이스로 해석해 충돌 오류 떠서 수정
+        {
+            num = 0;
+        }
+
+        Console.WriteLine(num);
     }
 }
