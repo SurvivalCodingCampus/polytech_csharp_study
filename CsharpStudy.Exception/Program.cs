a093d01d665ab595ef0cfa441d1df24ac22b1b6a
KOO TAEHAN <137146632+OmaryKoo@users.noreply.github.com>
2025-09-01 12:29:39 +0900

    feat.예외_연습문제1

---
+++ CsharpStudy.Exception/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CsharpStudy.Exception;
+
+class Program
+{
+    static void Main(string[] _)
+    {
+        var numString = "10.5"; // 소수점 문자열
+        int num = int.Parse(numString); 
+        Console.WriteLine(num); // 도달하지 못함
+    }
+}
