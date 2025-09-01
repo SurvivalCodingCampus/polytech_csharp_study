76d7c3d8529da529868d0c779a247ef72de4f6dd
Junsuk Oh <junsuk.oh@survivalcoding.com>
2025-09-01 10:23:14 +0900

    feat: 확장함수, Null Safety

--- FP2/Trader/Trader.cs
+++ FP2/Trader/Trader.cs
@@ -46,6 +46,33 @@ public class MainClass
             .Select(transaction => transaction.Trader.Name)
             .ToList()
             .ForEach(Console.WriteLine);
-            
+
+
+        // null safety :
+        List<string> transactions2 = new List<string>();
+        string? nullableFirst = transactions2.FirstOrDefault();
+
+        // string first = "기본값";
+        // if (nullableFirst != null)
+        // {
+        //     first = nullableFirst;
+        // }
+        
+        string first = nullableFirst ?? "기본값";
+        Console.WriteLine(first);
+
+        transactions2.Max();
+
+        // 글자수가 짝수인지
+        bool isEven = first.IsEven();
+    }
+}
+
+// 확장 함수
+public static class StringExtensions
+{
+    public static bool IsEven(this string str)
+    {
+        return str.Length % 2 == 0;
     }
 }
