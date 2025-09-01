8c61d2fa6f970ab90ecb316df014999efdc3dbf2
Dayeong <204299917+iiijjjijij@users.noreply.github.com>
2025-09-01 12:26:03 +0900

    feat: try-catch()문을 사용하여 예외 처리

--- CsharpStudy.Exception_File/Program.cs
+++ CsharpStudy.Exception_File/Program.cs
@@ -4,11 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        var numString = "10.5";
-        int num=int.Parse(numString);
-        
-        Console.WriteLine(num);
+        string? numString = null;
         // FormatException 발생
         // int.Parse()는 정수 문자열만 받아들임
+
+        try
+        {
+            int num=int.Parse(numString);
+            Console.WriteLine(num);
+        }
+        catch (ArgumentNullException argNull)
+        {
+            Console.WriteLine("값이 null 입니다.");
+        }
+        catch (FormatException notInt)
+        {
+            Console.WriteLine("값이 정수가 아닙니다.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("예상치 못 한 오류 : " + ex.Message);
+        }
+        finally
+        {
+            numString = "0";
+        }
     }
 }
