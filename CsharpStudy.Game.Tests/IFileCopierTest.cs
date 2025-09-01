1e4282575429e3d9096d360d4fe9de5448c51d5e
Hae1Won1 <h121581@naver.com>
2025-09-01 14:43:26 +0900

    feat : DefaultFileOperations test파일 작성

---
+++ CsharpStudy.Game.Tests/IFileCopierTest.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using CshapStudy.File;
+using NUnit.Framework;
+
+namespace CsharpStudy.Game.Tests;
+
+[TestFixture]
+[TestOf(typeof(IFileCopier))]
+public class IFileCopierTest
+{
+
+    [Test]
+    public void 기본파일_복사()
+    {
+        string text = "Hello World";
+        File.WriteAllText(@"C:\Users\goono\test.txt", text);
+        File.WriteAllText(@"C:\Users\goono\test2.txt",null);
+        string sourceFilePath = @"C:\Users\goono\test.txt"; 
+        string destinationFilePath = @"C:\Users\goono\test2.txt";
+        
+        DefaultFileOperations defaultFileOperations = new DefaultFileOperations();
+        defaultFileOperations.CopyFile(sourceFilePath, destinationFilePath);
+        
+        string result = File.ReadAllText(destinationFilePath);
+        Assert.That(result, Is.EqualTo(text));
+    }
+    [Test]
+    public void 여러줄_파일_복사()
+    {
+        string text = "Hello World\n 이것은 테스트 파일입니다.\n 옳은 출력을 확인합니다.\n20250901";
+        File.WriteAllText(@"C:\Users\goono\test.txt", text);
+        File.WriteAllText(@"C:\Users\goono\test2.txt",null);
+        string sourceFilePath = @"C:\Users\goono\test.txt"; 
+        string destinationFilePath = @"C:\Users\goono\test2.txt";
+        
+        DefaultFileOperations defaultFileOperations = new DefaultFileOperations();
+        defaultFileOperations.CopyFile(sourceFilePath, destinationFilePath);
+        
+        string result = File.ReadAllText(destinationFilePath);
+        Assert.That(result, Is.EqualTo(text));
+    }
+    
+    [Test]
+    public void Exception_FilePath_is_Wrong()
+    {
+        string destinationFilePath = @"C:\Users\goono\test2.txt";
+        
+        DefaultFileOperations defaultFileOperations = new DefaultFileOperations();
+        defaultFileOperations.CopyFile("?", destinationFilePath);
+    }
+}
