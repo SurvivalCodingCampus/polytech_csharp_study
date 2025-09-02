de9ba30fefc0557ea1028f05a1df2f9bfc14f906
Hae1Won1 <h121581@naver.com>
2025-09-02 11:47:07 +0900

    feat : class Department

---
+++ CsharpStudy.Data/Department.cs
@@ -0,0 +1,13 @@
+﻿namespace CsharpStudy.Data;
+
+public class Department
+{
+    public string Name { get; }
+    public Employee leader { get; }
+
+    public Department(string name, Employee leader)
+    {
+        Name = name;
+        this.leader = leader;
+    }
+}
