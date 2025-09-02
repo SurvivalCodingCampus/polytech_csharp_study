2ef6c5d135a3b39a577d5bcc2632e616acbe4702
zminsuck <125437750+zminsuck@users.noreply.github.com>
2025-09-02 13:25:24 +0900

    직렬화 & Json 코드 커밋

---
+++ CsharpStudy.Data/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using CsharpStudy.Data;
+
+// 리더/부서 인스턴스 생성
+var leader = new Employee("홍길동", 41);
+var dept = new Department("총무부", leader);
+
+// JSON 문자열로 직렬화
+string json = JsonSerializer.Serialize(dept);
+
+// 파일로 저장
+File.WriteAllText("company.json", json);
+Console.WriteLine("저장 완료: company.json");
