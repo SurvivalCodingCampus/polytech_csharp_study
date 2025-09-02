5c5541e2f79b4708720c73420af99aeb4534bf8f
Hae1Won1 <h121581@naver.com>
2025-09-02 11:46:41 +0900

    feat : Class Employee

---
+++ CsharpStudy.Data/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace CsharpStudy.Data;
+
+public class Employee
+{
+    public string Name { get; }
+    public int Age { get; }
+
+    public Employee(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
