fadc4728c427cec3f22ca89bda49113de1c31527
Junsuk Oh <junsuk.oh@survivalcoding.com>
2025-09-08 11:02:02 +0900

    feat: 비동기 프로그래밍

--- CsharpStudy.JsonSerialization/Program.cs
+++ CsharpStudy.JsonSerialization/Program.cs
@@ -30,6 +30,7 @@ class Program
 
     static void Main(string[] args)
     {
+        // Main Thread
         Department department = new Department(
             name: "총무부",
             leader: new Employee(
@@ -37,9 +38,37 @@ class Program
                 age: 41
             )
         );
-        
+
         string jsonString = JsonConvert.SerializeObject(department);
-        File.WriteAllText("department.json", jsonString);
-        Console.WriteLine("저장 완료");
+
+        // 다른 스레드 생성
+        File.WriteAllTextAsync("department.json", jsonString)
+            .ContinueWith(task =>
+            {
+                File.WriteAllTextAsync("hero.txt", "영웅 정보")
+                    .ContinueWith(task =>
+                    {
+                        File.WriteAllTextAsync("hero2.txt", "영웅2 정보")
+                            .ContinueWith(task => { Console.WriteLine("최종 완료"); });
+                    });
+                Console.WriteLine("저장 완료 : 2");
+            });
+
+        // 파일 저장이 1초 걸린다고 가정
+        Task.Delay(1000)
+            .ContinueWith(task =>
+            {
+                File.WriteAllTextAsync("hero3.txt", "영웅3 정보")
+                    .ContinueWith(task => { Console.WriteLine("hero3 완료"); });
+            });
+
+        // Main Thread
+        Console.WriteLine("저장 완료 : 1");
+
+        File.WriteAllTextAsync("hero4.txt", "영웅4 정보")
+            .ContinueWith(task => { Console.WriteLine("hero4 완료"); });
+
+        // Main Thread 안 끝나게 꼼수
+        Console.ReadLine();
     }
 }
