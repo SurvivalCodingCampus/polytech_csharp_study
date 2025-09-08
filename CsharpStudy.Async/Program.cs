990c0b019f82175cc7e9653c5c206cdf74a6354d
MJ37031 <rlaalswl0520@naver.com>
2025-09-08 11:01:16 +0900

    250908 수업: 비동기 프로그래밍

---
+++ CsharpStudy.Async/Program.cs
@@ -0,0 +1,110 @@
+﻿using System.Globalization;
+
+namespace CsharpStudy.Async;
+
+class Program
+{
+
+    static Task MyTaskAsync()
+    {
+        Task.Delay(3000)
+            .ContinueWith(task => Console.WriteLine("MyTask"));
+        return Task.CompletedTask;
+    }
+    
+    static async Task MyTaskAsync2()
+    {
+        await Task.Delay(3000);  // 순차실행
+        Console.WriteLine("MyTask2");
+    }
+
+    static async Task<int> GetInt1()
+    {
+        await Task.Delay(1000);
+        return 1;
+    }
+    static async Task<int> GetInt2()
+    {
+        await Task.Delay(1000);
+        return 2;
+    }
+    static async Task<int> GetInt3()
+    {
+        await Task.Delay(1000);
+        return 3;
+    }
+    static async Task<int> GetInt4()
+    {
+        await Task.Delay(1000);
+        return 4;
+    }
+    static async Task<int> GetInt5()
+    {
+        await Task.Delay(1000);
+        return 5;
+    }
+
+    static async Task Main(string[] args)
+    {
+        List<Task<int>> list = new List<Task<int>>();
+        
+        list.Add(GetInt1());
+        Console.WriteLine("1초");
+        list.Add(GetInt2());
+        Console.WriteLine("2초");
+        list.Add(GetInt3());
+        Console.WriteLine("3초");
+        list.Add(GetInt4());
+        Console.WriteLine("4초");
+        list.Add(GetInt5());
+        Console.WriteLine("5초");
+        
+        await Task.WhenAll(list);
+        Console.WriteLine("모든 작업 완료");
+    }
+    
+    static async Task AsyncExam(string[] args)
+    {
+        Console.WriteLine("MyTask start");
+        // MyTaskAsync();
+        await MyTaskAsync2();
+        Console.WriteLine("MyTask end");
+    }
+    
+    static async Task AsyncExam2(string[] args)  // 비동기 순차 실행
+    {
+        List<int> list = new List<int>();
+        
+        // await 때문에 5초 걸림
+        list.Add(await GetInt1());
+        Console.WriteLine("1초");
+        list.Add(await GetInt2());
+        Console.WriteLine("2초");
+        list.Add(await GetInt3());
+        Console.WriteLine("3초");
+        list.Add(await GetInt4());
+        Console.WriteLine("4초");
+        list.Add(await GetInt5());
+        Console.WriteLine("5초");
+    }
+    
+    static async Task AsyncExam3(string[] args)  // 병렬처리 구현
+    {
+        List<Task<int>> list = new List<Task<int>>();
+        
+        // await 때문에 5초 걸림
+        list.Add(GetInt1());
+        Console.WriteLine("1초");
+        list.Add(GetInt2());
+        Console.WriteLine("2초");
+        list.Add(GetInt3());
+        Console.WriteLine("3초");
+        list.Add(GetInt4());
+        Console.WriteLine("4초");
+        list.Add(GetInt5());
+        Console.WriteLine("5초");
+        
+        await Task.WhenAll(list);
+        Console.WriteLine("모든 작업 완료");
+    }
+}
--- CsharpStudy.Data/Test.cs
+++ CsharpStudy.Data/Test.cs
@@ -10,45 +10,57 @@ public class Employee
     {
         Name = name;
         Age = age;
+
     }
 }
 
 public class Department
 {
-    public string Name { get; }
-    public Employee leader { get; }
+        public string Name { get; }
+        public Employee leader { get; }
 
-    public Department(string name, Employee leader)
-    {
-        Name = name;
-        this.leader = leader;
-    }
+        public Department(string name, Employee leader)
+        {
+            Name = name;
+            this.leader = leader;
+        }
 }
 
 public class Test
 {
+    // Main Thread
     static void Main(string[] args)
     {
-        JsonDeserialization();
+        JsonSerialization();
     }
-    
+
     static void JsonSerialization()
     {
         // 직렬화
-        Employee employee = new Employee("홍길동", 41);
-        
-        string jsonString = JsonConvert.SerializeObject(employee);
-        Console.WriteLine(employee);
+        Department department = new Department("총무부", new Employee("홍길동", 41));
+
+        string jsonString = JsonConvert.SerializeObject(department);
+        Console.WriteLine(department);
         Console.WriteLine(jsonString);
-        File.WriteAllText("company.json", jsonString);
+        
+        // 다른 스레드 생성
+        File.WriteAllTextAsync("company.json", jsonString)
+            .ContinueWith(task =>
+            {
+                File.WriteAllTextAsync("hero.txt", "영웅정보");
+                Console.WriteLine("저장완료 : 2");
+            });
+         
+         // Main Thread
+        Console.WriteLine("저장완료 : 1");  // 얘가 더 먼저 진행
     }
-    
+
     static void JsonDeserialization()
     {
         // 역직렬화
         string jsonString = File.ReadAllText("company.json");
         Employee? employee = JsonConvert.DeserializeObject<Employee>(jsonString);
-        
+
         Console.WriteLine(employee);
     }
 }
