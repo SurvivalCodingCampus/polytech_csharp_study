3221e35a2284872c9963c2f3c6641edb35dd521f
jinsukuku <jinsukuku@gmail.com>
2025-09-09 15:32:43 +0900

    feat: DataSource 과제 시작 전

--- CsharpStudy.2hakgi/Assignment/DataSource/DataSource.cs
+++ CsharpStudy.2hakgi/Assignment/DataSource/DataSource.cs
@@ -11,12 +11,12 @@ public class DataSource
         var adults = people.Where(p => p.Age >= 19).ToList();
         Console.WriteLine("성인만 출력:");
         adults.ForEach(p => Console.WriteLine($"- {p.Name} ({p.Age}세)"));
-
+        
         // 2. 데이터를 추가하고 저장하는 로직
-        people.Add(new Person { Name = "김민지", Age = 15 });
+        people.Add(new Person { Name = "김민지", Age = 15 }); 
         await dataSource.SavePeopleAsync(people);
         Console.WriteLine("\n김민지 추가 후 저장 완료.");
-
+        
         // 3. 데이터를 삭제하는 로직
         var toDelete = people.FirstOrDefault(p => p.Name == "이민준");
         if (toDelete != null)
