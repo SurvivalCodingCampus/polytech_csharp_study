a20487866dc453d948a8cbb7482d9b5fdcf383bb
Younhee <officeryounhee@gmail.com>
2025-09-09 09:55:49 +0900

    Hotfix: modified Task.Delay() timing

--- CsharpStudy.Asynchronous/Entities/Crow.cs
+++ CsharpStudy.Asynchronous/Entities/Crow.cs
@@ -7,8 +7,8 @@ public class Crow : Bird
         for (var i = 0; i < 4; i++)
         {
             Console.WriteLine($"({TimeElapsed}ms) 까마귀 까악");
-            await Task.Delay(2000);
             TimeElapsed += 2000;
+            await Task.Delay(2000);
         }
         
     }
--- CsharpStudy.Asynchronous/Entities/Sparrow.cs
+++ CsharpStudy.Asynchronous/Entities/Sparrow.cs
@@ -7,8 +7,8 @@ public class Sparrow : Bird
         for (var i = 0; i < 4; i++)
         {
             Console.WriteLine($"({TimeElapsed}ms) 참새 짹짹");
-            await Task.Delay(3000);
             TimeElapsed += 3000;
+            await Task.Delay(3000);
         }
 
     }
