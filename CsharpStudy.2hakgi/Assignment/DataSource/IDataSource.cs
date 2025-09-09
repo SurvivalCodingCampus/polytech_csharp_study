02cac3fbec2548850f6080a3b56fef62c1589ce5
jinsukuku <jinsukuku@gmail.com>
2025-09-09 15:36:10 +0900

    feat: IDataSource 인터페이스에 GetPeopleAsync, SavePeopleAsync 메서드 선언

--- CsharpStudy.2hakgi/Assignment/DataSource/IDataSource.cs
+++ CsharpStudy.2hakgi/Assignment/DataSource/IDataSource.cs
@@ -1,6 +1,9 @@
 namespace CsharpStudy._2hakgi.Assignment.DataSource;
 
 public interface IDataSource
-{
-    
+{ 
+    abstract Task<List<Person>> GetPeopleAsync();
+
+    abstract Task SavePeopleAsync(Person [] people);
+
 }
