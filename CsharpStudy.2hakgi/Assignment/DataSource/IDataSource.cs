07237f0148587fad422d794f66d4194cb0485c67
jinsukuku <jinsukuku@gmail.com>
2025-09-09 14:55:49 +0900

    feat: DataSource 과제 시작 전

---
+++ CsharpStudy.2hakgi/Assignment/DataSource/IDataSource.cs
@@ -0,0 +1,6 @@
+namespace CsharpStudy._2hakgi.Assignment.DataSource;
+
+public interface IDataSource
+{
+    
+}
---
+++ CsharpStudy.2hakgi/Assignment/DataSource/JsonFileDataSource.cs
@@ -0,0 +1,6 @@
+namespace CsharpStudy._2hakgi.Assignment.DataSource;
+
+public class JsonFileDataSource
+{
+    
+}
