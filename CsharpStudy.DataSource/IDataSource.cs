f164da6c4225e2339760dc2c2f3b82c313f0a40d
Hae1Won1 <h121581@naver.com>
2025-09-09 14:32:47 +0900

    feat : IDataSource 인터페이스 생성

---
+++ CsharpStudy.DataSource/IDataSource.cs
@@ -0,0 +1,9 @@
+﻿namespace CsharpStudy.DataSource;
+
+public interface IDataSource
+{
+    public Task<List<Person>> GetPeopleAsync();
+    public Task SavePeopleAsync(List<Person> people);
+    public void Add(Person person);
+    public void Remove(Person person);
+}
