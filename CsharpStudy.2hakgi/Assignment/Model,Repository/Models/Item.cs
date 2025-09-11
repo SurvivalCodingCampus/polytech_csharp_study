03bd5a7cb9d34e373d757b81ab9e97a9cfa73071
jinsukuku <jinsukuku@gmail.com>
2025-09-11 14:22:49 +0900

    feat: Item 클래스 프로퍼티 및 생성자 추가

--- CsharpStudy.2hakgi/Assignment/Model,Repository/Models/Item.cs
+++ CsharpStudy.2hakgi/Assignment/Model,Repository/Models/Item.cs
@@ -2,5 +2,14 @@ namespace CsharpStudy._2hakgi.Assignment.Model_Repository.Models;
 
 public class Item
 {
+    public string ItemID { get; set; }   
+    public string ItemName { get; set; }   
+    public int ItemCount { get; set; }   
     
+    public Item(string itemId, string itemName, int itemCount)
+    {
+        ItemID = itemId;
+        ItemName = itemName;
+        ItemCount = itemCount;
+    }
 }
