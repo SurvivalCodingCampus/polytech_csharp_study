07ee6eab4eba1c0b1ffcd081883df5a0e8ad2d59
jinsukuku <jinsukuku@gmail.com>
2025-09-11 16:26:17 +0900

    feat: MockItemDataSource 테스트 케이스1 을 위한 아이템 Sword, Shield 추가

--- CsharpStudy.2hakgi/Assignment/Model,Repository/Data/DataSources/MockItemDataSource.cs
+++ CsharpStudy.2hakgi/Assignment/Model,Repository/Data/DataSources/MockItemDataSource.cs
@@ -5,6 +5,8 @@ namespace CsharpStudy._2hakgi.Assignment.Model_Repository.Data.DataSources;
 public class MockItemDataSource: IItemDataSource
 {
     private List<Item> _items = [
+        new Item(1, "Sword", 10),
+        new Item(2, "Shield", 10)
     ];
 
     public async Task<List<Item>> LoadAllItemsAsync()
