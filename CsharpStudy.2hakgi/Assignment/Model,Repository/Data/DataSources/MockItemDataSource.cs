7628f7b9e0a53a37327a91954a044d517d777c90
jinsukuku <jinsukuku@gmail.com>
2025-09-11 15:14:56 +0900

    feat: MockItemDataSource 추가

---
+++ CsharpStudy.2hakgi/Assignment/Model,Repository/Data/DataSources/MockItemDataSource.cs
@@ -0,0 +1,18 @@
+using CsharpStudy._2hakgi.Assignment.Model_Repository.Models;
+
+namespace CsharpStudy._2hakgi.Assignment.Model_Repository.Data.DataSources;
+
+public class MockItemDataSource: IItemDataSource
+{
+    private List<Item> _items = [];
+
+    public async Task<List<Item>> LoadAllItemsAsync()
+    {
+        return _items;
+    }
+
+    public async Task SaveAllItemsAsync(List<Item> items)
+    {
+        throw new NotImplementedException();
+    }
+}
