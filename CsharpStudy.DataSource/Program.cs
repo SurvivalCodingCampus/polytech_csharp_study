8fb390468471489d9fa0edf880a213380db882af
Junsuk Oh <junsuk.oh@survivalcoding.com>
2025-09-11 09:43:03 +0900

    feat: 데이터 소스 프로젝트 추가
    - `CsharpStudy.DataSource` 프로젝트 생성: JSON 데이터 소스 구현.
    - 인터페이스 `IDataSource` 정의 및 JSON 기반 구현 클래스 `JsonFileDataSource` 추가.
    - `Person` 모델 클래스 정의.
    - 프로그램을 통해 JSON 데이터 읽기, 추가, 삭제 및 저장 동작 구현.
    - Newtonsoft.Json 패키지 추가 및 솔루션 파일에 새 프로젝트 참조 포함.

---
+++ CsharpStudy.DataSource/Program.cs
@@ -0,0 +1,41 @@
+﻿using CsharpStudy.DataSource.DataSources;
+using CsharpStudy.DataSource.Interfaces;
+using CsharpStudy.DataSource.Models;
+
+namespace CsharpStudy.DataSource;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        IDataSource dataSource = new JsonFileDataSource("people.json");
+
+        // 읽기
+        List<Person> people = await dataSource.GetPeopleAsync();
+        List<Person> adults = people.Where(p => p.Age >= 19).ToList();
+        adults.ForEach(p => Console.WriteLine($"{p.Name} ({p.Age})"));
+
+        // 추가
+        people.Add(new Person("홍길동", 10));
+        await dataSource.SavePeopleAsync(people);
+        Console.WriteLine("추가 후 저장 완료");
+
+        // 삭제
+        await dataSource.SavePeopleAsync(people.Where(p => p.Name != "이민준").ToList());
+
+
+        Person person = new Person("aa", 10);
+
+        // Person person2 = new Person();
+        // person2.Name = "aaa";
+        // person2.Age = 100;
+        // 
+        // Person person2 = new Person
+        // {
+        //     Name = "aaa",
+        //     Age = 100
+        // };
+
+        Console.WriteLine("Hello, World!");
+    }
+}
