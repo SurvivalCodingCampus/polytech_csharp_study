9c2fa1cbc60b9bea03e216e2bdf7d27b22137189
MJ37031 <rlaalswl0520@naver.com>
2025-09-11 15:58:02 +0900

    feat: 과제 3. 테스트를위해 MockItemDataSource : IItemDataSource 를 작성

---
+++ CsharpStudy.Repository/Data/DataSources/MockItemDataSource.cs
@@ -0,0 +1,17 @@
+using CsharpStudy.Repository.Models;
+
+namespace CsharpStudy.Repository.Data.DataSources;
+
+public class MockItemDataSource :  IItemDataSource
+{
+    private List<Item> _items = [];
+    public async Task<List<Item>> LoadAllItemsAsync()
+    {
+        return _items;
+    }
+
+    public async Task SaveAllItemsAsync(List<Item> items)
+    {
+        _items = items;
+    }
+}
