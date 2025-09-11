f743300ed9733887c735f34828dcd349d6c17808
jinsukuku <jinsukuku@gmail.com>
2025-09-12 08:35:04 +0900

    refactoring: 폴더 구조 정리

---
+++ CsharpStudy.Repository/Data/DataSources/MockItemDataSource.cs
@@ -0,0 +1,24 @@
+using CsharpStudy._2hakgi.Assignment.Model_Repository.Models;
+
+namespace CsharpStudy._2hakgi.Assignment.Model_Repository.Data.DataSources;
+
+public class MockItemDataSource: IItemDataSource
+{
+    private List<Item> _items = [
+        new Item(1, "Sword", 1),
+        new Item(2, "Shield", 1)
+    ];
+
+    public async Task<List<Item>> LoadAllItemsAsync()
+    {
+        // async 키워드만 있으면 사실상 동기로 작동.
+        // 비동기 작동을 원하면 await 넣어주기
+        await Task.Yield();
+        return _items;
+    }
+
+    public async Task SaveAllItemsAsync(List<Item> items)
+    {
+        _items = items;
+    }
+}
---
+++ CsharpStudy.Repository/Models/Item.cs
@@ -0,0 +1,15 @@
+namespace CsharpStudy._2hakgi.Assignment.Model_Repository.Models;
+
+public class Item
+{
+    public int ItemId { get; set; }   
+    public string ItemName { get; set; }   
+    public int ItemCount { get; set; }   
+    
+    public Item(int itemId, string itemName, int itemCount)
+    {
+        ItemId = itemId;
+        ItemName = itemName;
+        ItemCount = itemCount;
+    }
+}
