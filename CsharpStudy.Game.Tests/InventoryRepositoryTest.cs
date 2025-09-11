df5af5404872d1c4f65c929cc342ec181344583d
Hae1Won1 <h121581@naver.com>
2025-09-11 16:58:14 +0900

    fix : 오타 수정

--- CsharpStudy.Game.Tests/InventoryRepositoryTest.cs
+++ CsharpStudy.Game.Tests/InventoryRepositoryTest.cs
@@ -59,7 +59,7 @@ public class InventoryRepositoryTest
         items = await inventory.GetItemsAsync();
         // 2. Potion 목록 확인
         Assert.That(items[2].Name, Is.EqualTo("Potion"));
-        Assert.That(await inventory.GetItemByldAsync(2), Is.EqualTo(itemAdd));
+        Assert.That(await inventory.GetItemByIdAsync(2), Is.EqualTo(itemAdd));
 
         // 3. 아이템 종류 수 확인
         Assert.That(items.Count, Is.EqualTo(3));
@@ -74,19 +74,19 @@ public class InventoryRepositoryTest
 
         MockItemDataSource mockItemDataSource = new MockItemDataSource();
         await mockItemDataSource.SaveAllItemsAsync(items);
-        
+
         // maxStack 20
         InventoryRepository inventory = new InventoryRepository(mockItemDataSource, 2, 20);
         await inventory.GetItemsAsync();
-        
+
         // 목록에 있는 아이템 다시 저장
         bool isAddItem = await inventory.AddItemAsync(items[0]);
-        
+
         // Then
         // 1. 저장 성공여부
         Assert.That(isAddItem, Is.True);
         items = await inventory.GetItemsAsync();
-        
+
         // 2. 추가된 개수 확인
         Assert.That(items[0].Count, Is.EqualTo(2));
     }
@@ -101,7 +101,7 @@ public class InventoryRepositoryTest
 
         MockItemDataSource mockItemDataSource = new MockItemDataSource();
         await mockItemDataSource.SaveAllItemsAsync(items);
-        
+
         InventoryRepository inventory = new InventoryRepository(mockItemDataSource, 2, 2);
         await inventory.GetItemsAsync();
 
@@ -113,36 +113,35 @@ public class InventoryRepositoryTest
         // 1. 저장 성공여부
         Assert.That(isAddItem, Is.False);
         items = await inventory.GetItemsAsync();
-        
+
         // 2. 아이템 종류 수 확인
         Assert.That(items.Count, Is.EqualTo(2));
-        
+
         // 3. Potion 목록 확인
-        Assert.That(await inventory.GetItemByldAsync(itemAdd.Id), Is.Null);
+        Assert.That(await inventory.GetItemByIdAsync(itemAdd.Id), Is.Null);
     }
 
     [Test]
     public async Task 아이템_개수_증가_False_maxStack()
     {
-        
         // Glven
         List<Item> items = new List<Item>();
         items.Add(new Item(0, "Potion", 99));
 
         MockItemDataSource mockItemDataSource = new MockItemDataSource();
         await mockItemDataSource.SaveAllItemsAsync(items);
-        
+
         // maxStack 20
         InventoryRepository inventory = new InventoryRepository(mockItemDataSource, 2, 99);
         await inventory.GetItemsAsync();
-        
+
         // 목록에 있는 아이템 다시 저장
         bool isAddItem = await inventory.AddItemAsync(items[0]);
-        
+
         // Then
         // 1. 저장 성공여부
         Assert.That(isAddItem, Is.True);
-        
+
         // 2. 추가된 개수 확인
         items = await inventory.GetItemsAsync();
         Assert.That(items[0].Count, Is.EqualTo(99));
