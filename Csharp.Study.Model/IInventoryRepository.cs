848bb98fde5f9f107ea156277099e1cb623256fc
KOO TAEHAN <137146632+OmaryKoo@users.noreply.github.com>
2025-09-11 15:14:48 +0900

    feat. Model 과제2 + IIventoryRepository 인터페이스 ~Task<Item> -> <Item?> 수정

--- Csharp.Study.Model/IInventoryRepository.cs
+++ Csharp.Study.Model/IInventoryRepository.cs
@@ -2,7 +2,9 @@
 
 public interface IInventoryRepository
 {
-    Task<List<Item>> GetItemsAsync();        // 모든 아이템 가져오기
-    Task<Item> GetItemByIdAsync(int itemId);   // 특정 아이템 검색
-    Task<bool> AddItemAsync(Item item);    // 아이템 추가
+    Task<List<Item>> GetItemsAsync(); // 모든 아이템 가져오기
+
+    Task<Item?> GetItemByIdAsync(int itemId); // 특정 아이템 검색
+
+    Task<bool> AddItemAsync(Item item); // 아이템 추가
 }
---
+++ Csharp.Study.Model/InventoryRepository.cs
@@ -0,0 +1,33 @@
+﻿namespace Csharp.Study.Model;
+
+public class InventoryRepository : IInventoryRepository
+{
+    private IItemDataSource _ds;
+    private int _maxSlot;
+    private int _maxStack;
+
+    public InventoryRepository(IItemDataSource ds, int maxSlot, int maxStack)
+    {
+        _ds = ds ?? throw new ArgumentNullException(nameof(ds)); // 만약 ds가 null이면 예외, null이 아니면 _ds 할당
+        if (maxSlot <= 0) throw new ArgumentOutOfRangeException(nameof(maxSlot));
+        if (maxStack <= 0) throw new ArgumentOutOfRangeException(nameof(maxStack));
+
+        _maxSlot  = maxSlot;
+        _maxStack = maxStack;
+    }
+
+    public Task<List<Item>> GetItemsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Item?> GetItemByIdAsync(int itemId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> AddItemAsync(Item item)
+    {
+        throw new NotImplementedException();
+    }
+}
