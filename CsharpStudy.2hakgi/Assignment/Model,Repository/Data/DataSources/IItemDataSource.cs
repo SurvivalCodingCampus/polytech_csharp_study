1169ab424e8ae04ba41df747af3518fe91fa4083
jinsukuku <jinsukuku@gmail.com>
2025-09-11 14:38:41 +0900

    feat: IItemDataSource 인터페이스 LoadAllItemsAsync(), SaveAllItemsAsync()  선언

--- CsharpStudy.2hakgi/Assignment/Model,Repository/Data/DataSources/IItemDataSource.cs
+++ CsharpStudy.2hakgi/Assignment/Model,Repository/Data/DataSources/IItemDataSource.cs
@@ -1,6 +1,11 @@
+using CsharpStudy._2hakgi.Assignment.Model_Repository.Models;
 namespace CsharpStudy._2hakgi.Assignment.Model_Repository.Data.DataSources;
 
 public interface IItemDataSource
 {
-    
+    // Task<List<Item>> LoadAllItemsAsync() : 아이템 데이터를 읽어오는 메서드
+    abstract Task<List<Item>> LoadAllItemsAsync();
+    // Task SaveAllItemsAsync(List<Item> items) : 아이템 데이터를 저장하는 메서드 데이터를 읽어오는 메서드
+    abstract Task SaveAllItemsAsync(List<Item> items);
+
 }
