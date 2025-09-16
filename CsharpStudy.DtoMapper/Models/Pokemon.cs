4e552a50091a3bd993efdaf7d983911c028dda16
MJ37031 <rlaalswl0520@naver.com>
2025-09-16 11:05:15 +0900

    Result 패턴

--- CsharpStudy.DtoMapper/Models/Pokemon.cs
+++ CsharpStudy.DtoMapper/Models/Pokemon.cs
@@ -2,6 +2,9 @@ using CsharpStudy.DtoMapper.DTOs;
 
 namespace CsharpStudy.DtoMapper.Models;
 
+//public record Pokemon(int Id, string Name, string ImageUrl, List<AbilityWrapperDto> Abilities);
+
+
 public class Pokemon
 {
     public int Id { get; }
