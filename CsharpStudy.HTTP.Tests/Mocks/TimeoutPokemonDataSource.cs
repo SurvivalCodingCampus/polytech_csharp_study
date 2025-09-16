43c635b25c671e128856cee66dd38ac9d4013d32
KOO TAEHAN <137146632+OmaryKoo@users.noreply.github.com>
2025-09-16 15:42:46 +0900

    feat. 테스트코드

---
+++ CsharpStudy.HTTP.Tests/Mocks/TimeoutPokemonDataSource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using CsharpStudy.HTTP.DataSources;
+using CsharpStudy.HTTP.DTOs;
+
+namespace CsharpStudy.HTTP.Tests.Mocks 
+{
+    public sealed class TimeoutPokemonDataSource : IPokemonApiDataSource
+    {
+        public Task<PokemonDto?> GetPokemonAsync(string name)
+            => throw new TimeoutException("Mock Timeout");
+    }
+}
---
+++ CsharpStudy.HTTP.Tests/Repositories/PokemonRepositoryTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CsharpStudy.HTTP.Common;                 
+using CsharpStudy.HTTP.Repositories;          
+using CsharpStudy.HTTP.Tests.Mocks;
+
+namespace CsharpStudy.HTTP.Tests.Repositories 
+{
+    [TestFixture]
+    public class PokemonRepositoryTests
+    {
+        [Test]
+        public async Task GetByNameAsync_AlwaysTimeout_ReturnsTimeoutError()
+        {
+            // Arrange
+            var repo = new PokemonRepository(new TimeoutPokemonDataSource());
+
+            // Act
+            var (result, data) = await repo.GetByNameAsync("pikachu");
+
+            // Assert
+            Assert.That(result, Is.TypeOf<Result.Failure>());
+            var failure = (Result.Failure)result;
+            Assert.That(failure.Error.Kind, Is.EqualTo(PokemonError.Timeout));
+        }
+
+        [Test]
+        public async Task GetByNameAsync_AlwaysJsonError_ReturnsUnknownError()
+        {
+            // Arrange
+            var repo = new PokemonRepository(new JsonErrorPokemonDataSource());
+
+            // Act
+            var (result, data) = await repo.GetByNameAsync("pikachu");
+
+            // Assert
+            Assert.That(result, Is.TypeOf<Result.Failure>());
+            var failure = (Result.Failure)result;
+            Assert.That(failure.Error.Kind, Is.EqualTo(PokemonError.Unknown));
+        }
+    }
+}
